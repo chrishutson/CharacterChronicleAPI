c7e0f1f90e0d6e9db1556c24d72da700813c16e7
Chris Hutson <chris.hutson@gmail.com>
2025-03-05 20:50:57 -0600

    Got a working controller to test things out

--- CharacterChronicleAPI/Models/DandD5ECharacter.cs
+++ CharacterChronicleAPI/Models/DandD5ECharacter.cs
@@ -5,6 +5,7 @@ namespace CharacterChronicleAPI.Models
 {
     public class DandD5ECharacter
     {
+        public long Id { get; set; }
         public string? characterName { get; set; }
         public int characterLevel { get; set; }
         public int currentXP { get; set; }
@@ -14,7 +15,7 @@ namespace CharacterChronicleAPI.Models
 
         public ISpecies? species;
         public ICharacterClass? characterClass;
-        public IAbilityScores? abilityScores;        
+        public IAbilityScores? abilityScores;
         public Dictionary<string, IEquipment>? equipment;
         public AlignmentEnum? alignment;
         public List<string>? learnedLanguages;
